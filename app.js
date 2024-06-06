1e40be93e4691539056ca3ebe8be3feaf7cec723
rasdhanaDawood <rasdhanadawood@gmail.com>
2024-06-06 09:23:59 +0300

    hosting

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ app.use("/admin", adminRoute);
 const productRoute = require("./routes/productRoute");
 app.use("/", productRoute);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
